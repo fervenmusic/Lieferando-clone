e3bda0c8e76005c0775d4eeccf3b4a7a711cf96b
Ferven <120587289+fervenmusic@users.noreply.github.com>
2023-11-20 16:15:46 +0100

    first com

---
+++ lieferando/script.js
@@ -0,0 +1,387 @@
+let menus = [
+    {
+        "name": "Pizza Schinken",
+        "description": "mit Käse und Hinterschinken",
+        "menu-image": "img/schinken.jpg",
+        "price": 9.99,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Salami",
+        "description": "mit Käse und Rindersalami",
+        "menu-image": "img/salami.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Bombay",
+        "description": "mit Ananas, Hähnchenbrustfilet, Brocolli und Currysauce",
+        "menu-image": "img/bombay.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Margherita",
+        "description": "mit extra Mozzarella ",
+        "menu-image": "img/cheese.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Tuna",
+        "description": "mit extra viel Thunfisch und roten Zwiebeln ",
+        "menu-image": "img/tuna.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Waikiki",
+        "description": "mit Prosciutto Cotto, Ananas und extra viel Käse ",
+        "menu-image": "img/waikiki.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Pizza Caprese",
+        "description": "mit frischen Tomaten, Mozzarella-Kugeln und Basilikum-Pesto",
+        "menu-image": "img/caprese.jpg",
+        "price": 9.79,
+        "category": ['Pizza'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "8 Pizzabrötchen Schinken",
+        "description": "mit Schinken und Käse",
+        "menu-image": "img/pizzabread2.jpg",
+        "price": 9.79,
+        "category": ['Rolls'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "8 Pizzabrötchen Spinat",
+        "description": "mit Spinat und Käse",
+        "menu-image": "img/pizzabread.jpg",
+        "price": 9.79,
+        "category": ['Rolls'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Salat Caesar's Chicken",
+        "description": "Römersalat mit Cherrytomaten, Hähnchenbrustfilet, italienischem Hartkäse und Croûtons",
+        "menu-image": "img/saladchicken.jpg",
+        "price": 8.99,
+        "category": ['Salat'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Salat Deluxe",
+        "description": "gemischter Salat mit Rucola, Cherrytomaten, Gurken, Prosciutto Cotto (Kochhinterschinken (zerkleinert)) , Champignons, Mais und Mozzarella. Unsere Empfehlung: American-Dressing (mit Estragon verfeinert) (100ml)",
+        "menu-image": "img/saladdeluxe.jpg",
+        "price": 8.99,
+        "category": ['Salat'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Salat Mini",
+        "description": "kleiner gemischter Salat mit Rucola, Gurken, Mais, Cherrytomaten und Croutons, mit Joghurt-Limetten-Dressing (100ml)",
+        "menu-image": "img/saladmini.jpg",
+        "price": 9.79,
+        "category": ['Salat'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Salat Green Garden",
+        "description": "gemischter Salat mit Rucola, Mozzarellakugeln, Crôutons, Cherrytomaten, Gurken, Frühlingszwiebeln und Champignons",
+        "menu-image": "img/saladgreen.jpg",
+        "price": 8.99,
+        "category": ['Salat'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Wrap Veggie",
+        "description": "Weizentortilla mit Kräuterremoulade, Romana Salat, Basilikum-Pesto, Tomaten und Mozzarella gerieben",
+        "menu-image": "img/wrapveggie.jpg",
+        "price": 9.79,
+        "category": ['Snacks'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Wrap Classic Chicken",
+        "description": "Weizentortilla mit Römersalat, Tomaten, Hähnchenbrustfilet, italienischem Hartkäse, Croutons und Kräuterremoulade",
+        "menu-image": "img/wrapchicken.jpg",
+        "price": 9.79,
+        "category": ['Snacks'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Wrap Italian Chicken",
+        "description": "Weizentortilla mit Salatmix und Rucola, geriebenem Mozzarella, in Basilikum-Pesto mariniertem Hähnchenbrustfilet, italienischem Hartkäse und Kräuterremoulade. Kalt serviert.",
+        "menu-image": "img/wrapchicken2.jpg",
+        "price": 9.79,
+        "category": ['Snacks'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Ben & Jerry's Cookie Dough",
+        "description": "Vanille-Eiscreme mit Schokochips-Plätzchenstücke",
+        "menu-image": "img/benjerryscookie.png",
+        "price": 2.99,
+        "category": ['Dessert'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Ben & Jerrys Strawberry Cheesecake",
+        "description": "Erdbeer- & Käsekuchen-Eiscreme mit Erdbeerstückchen und einem Keksstrudel",
+        "menu-image": "img/benjerrysberry.png",
+        "price": 2.99,
+        "category": ['Dessert'],
+        "quantity": 0,
+        "total": 0.00
+    },
+    {
+        "name": "Cookie Bits",
+        "description": "3 Kekse mit Schokoladenstückchen",
+        "menu-image": "img/cookie.jpg",
+        "price": 1.99,
+        "category": ['Dessert'],
+        "quantity": 0,
+        "total": 0.00
+    },
+];
+
+function addClass(id, className) {
+    document.getElementById(id).classList.add(className);
+}
+
+function removeClass(id, className) {
+    document.getElementById(id).classList.remove(className);
+}
+
+function closeBasket() {
+    removeClass('basket-cont2', 'hide');
+    removeClass('switchMobile', 'basket-mobile');
+    addClass('switchMobile', 'basket');
+    addClass('mobilePayButton', 'hide');
+    addClass('fillBasket', 'hide');
+}
+
+function backToMainPage() {
+    addClass('switchMobile', 'basket');
+    removeClass('switchMobile', 'basket-mobile');
+    removeClass('mobilePayButton', 'hide');
+}
+
+function openBasket() {
+    removeClass('switchMobile', 'hide');
+    removeClass('switchMobile', 'basket');
+    addClass('switchMobile', 'basket-mobile');
+    addClass('mobilePayButton', 'hide');
+}
+
+function resetBasket() {
+    for (let i = 0; i < menus.length; i++) {
+        menus[i].quantity = 0;
+        menus[i].total = 0.00;
+    }
+    document.getElementById('basket').innerHTML = '';
+}
+
+function groupByCategory(menus) {
+    const categoryGroups = {};
+
+    menus.forEach(menu => {
+        menu.category.forEach(category => {
+            if (!categoryGroups[category]) {
+                categoryGroups[category] = [];
+            }
+            categoryGroups[category].push(menu);
+        });
+    });
+
+    return categoryGroups;
+}
+
+function showPage() {
+    addClass('popupPay', 'hide');
+    removeClass('popupPay', 'display');
+    resetBasket();
+    addClass('side-basket', 'hide');
+    removeClass('fillBasket', 'hide');
+    removeClass('switchMobile', 'basket-mobile');
+}
+
+function popupPay() {
+    removeClass('popupPay', 'hide');
+    addClass('popupPay', 'display');
+    setTimeout(showPage, 4000);
+    removeClass('footerCont', 'footerCont');
+    closeBasket();
+}
+
+function calculateSum() {
+    let sum = 0;
+    for (let i = 0; i < menus.length; i++) {
+        sum += menus[i].total;
+    }
+    return sum;
+}
+
+function toggleBasketVisibility(sum) {
+    let sideBasket = document.getElementById('side-basket');
+    let fillBasket = document.getElementById('fillBasket');
+
+    if (sum > 0) {
+        sideBasket.classList.remove('hide');
+        fillBasket.classList.add('hide');
+    } else {
+        sideBasket.classList.add('hide');
+        fillBasket.classList.remove('hide');
+    }
+}
+
+function updateFinalCosts(sum) {
+    let subtotal = sum;
+    let deliveryCost = 3.95;
+    let finalSum = sum + deliveryCost;
+
+    document.getElementById('sum').innerHTML = finalSum.toFixed(2).replace('.', ',') + '€';
+    document.getElementById('pay-button').innerHTML = 'Bezahlen ' + '(' + finalSum.toFixed(2).replace('.', ',') + '€)';
+    document.getElementById('pay-button-mobile').innerHTML = 'Warenkorb ' + '(' + finalSum.toFixed(2).replace('.', ',') + '€)';
+    document.getElementById('subtotal').innerHTML = subtotal.toFixed(2).replace('.', ',') + '€';
+    document.getElementById('delivery-cost').innerHTML = deliveryCost.toFixed(2).replace('.', ',') + '€';
+}
+
+function finalSum() {
+    let sum = calculateSum();
+    toggleBasketVisibility(sum);
+    updateFinalCosts(sum);
+}
+
+function delMenuNumber(index) {
+    const menu = menus[index];
+    if (menu.quantity > 0) {
+        menu.quantity--;
+        menu.total -= menu.price;
+        if (menu.quantity === 0) {
+            document.getElementById(`basket-item-container${index}`).remove();
+            if (document.querySelectorAll('.basket-item-container').length === 0) {
+                removeClass('footerCont', 'footerCont');
+                closeBasket();
+            }
+        } else {
+            document.getElementById(`item-quantity${index}`).innerHTML = menu.quantity;
+            document.getElementById(`item-total${index}`).innerHTML = `${menu.total.toFixed(2).replace('.', ',')}€`;
+        }
+    }
+    finalSum();
+}
+
+function addMenuNumber(index) {
+    const menu = menus[index]
+    menu.quantity++;
+    menu.total += menu.price;
+    document.getElementById(`item-quantity${index}`).innerHTML = menu.quantity;
+    document.getElementById(`item-total${index}`).innerHTML = `${menu.total.toFixed(2).replace('.', ',')}€`;
+    finalSum()
+}
+
+function addMenuContent(index, menu) {
+    document.getElementById(`item-quantity${index}`).innerHTML = menu.quantity;
+    document.getElementById(`item-total${index}`).innerHTML = `${menu.total.toFixed(2).replace('.', ',')}€`;
+    removeClass('mobilePayButton', 'hide');
+    addClass('footerCont', 'footerCont');
+}
+
+function addMenu(index) {
+    let content = document.getElementById('basket');
+    const menu = menus[index];
+
+    if (menu.quantity > 0) {
+        menu.quantity++;
+        menu.total += menu.price;
+    } else {
+        menu.quantity = 1;
+        menu.total = menu.price;
+        content.innerHTML += createBasketHTML(index, menu);
+    }
+
+    addMenuContent(index, menu);
+    finalSum();
+}
+
+function render() {
+    const categoryGroups = groupByCategory(menus);
+    let content = document.getElementById('content-menu');
+
+    for (let category in categoryGroups) {
+        let categoryHTML = `<h2 id="${category}">${category}</h2>`;
+
+        let menusHTML = '';
+        for (let i = 0; i < categoryGroups[category].length; i++) {
+            menusHTML += createRenderHTML(categoryGroups[category][i]);
+        }
+
+        content.innerHTML += categoryHTML + menusHTML;
+    }
+}
+
+function createBasketHTML(index, menu) {
+    return `
+    <div class=basket-item-container id="basket-item-container${index}">
+        <div class="basket-text" id="basket-item${index}">
+            <span id="item-quantity${index}">${menu['quantity']}</span>
+            <span>${menu['name']}</span>
+            <div>
+                <span id="item-total${index}">${menu['total'].toFixed(2)}€</span>
+            </div>
+        </div>
+        <div class="minus-add">
+            <img src="icons/minus.svg" onclick="delMenuNumber(${index})" class="basketAdd">
+            <img src="icons/add.svg" onclick="addMenuNumber(${index})" class="basketAdd">
+        </div>
+    </div>`;
+}
+
+function createRenderHTML(menu) {
+    return `
+        <div class="menu-box">
+            <div class="menu-text-box">
+                <div class="word-break">
+                    <h3>${menu['name']}</h3>
+                    <p>${menu['description']}</p>
+                </div>
+                <b><span>${menu['price'].toFixed(2).replace('.', ',')}€</span></b>
+            </div>
+            <div class="menu-img-box">
+                <div class="align-menu-img-box">
+                    <img src="${menu['menu-image']}" alt="menu-image" class="menu-image">
+                </div>
+                <div>
+                    <img src="icons/plus.svg" onclick="addMenu(${menus.indexOf(menu)})" class="plus">
+                </div>
+            </div>
+        </div>`;
+}
